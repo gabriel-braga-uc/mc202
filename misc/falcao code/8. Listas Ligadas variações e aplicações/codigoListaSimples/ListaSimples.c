3f81ee245c2bf56bec1ce0236e2b8cee58a647d2
gabriel-braga-uc <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-04-21 00:50:10 -0300

    23
    1

--- misc/falcao code/8. Listas Ligadas variações e aplicações/codigoListaSimples/ListaSimples.c
+++ misc/falcao code/8. Listas Ligadas variações e aplicações/codigoListaSimples/ListaSimples.c
@@ -1,19 +1,20 @@
 #include "ListaSimples.h"
- 
+
 bool ListaSimplesVazia(ListaSimples *inicio)
 {
-  if (inicio == NULL) 
-    return(true);
+  if (inicio == NULL)
+    return (true);
   else
-    return(false);
+    return (false);
 }
 
 void ImprimeListaSimples(ListaSimples *inicio)
 {
 
-  while (!ListaSimplesVazia(inicio)){
-    printf("%d -->",inicio->elem);
-    inicio = inicio->prox; 
+  while (!ListaSimplesVazia(inicio))
+  {
+    printf("%d -->", inicio->elem);
+    inicio = inicio->prox;
   }
 }
 
@@ -21,96 +22,106 @@ bool BuscaElementoListaSimples(ListaSimples *inicio, int elem, NoListaSimples **
 {
 
   /* preencher */
-  
 }
 
-bool BuscaUltimoElementoListaSimples(ListaSimples *inicio, NoListaSimples **pos, NoListaSimples **ant){
+bool BuscaUltimoElementoListaSimples(ListaSimples *inicio, NoListaSimples **pos, NoListaSimples **ant)
+{
 
-  *ant = NULL; *pos = inicio;
+  *ant = NULL;
+  *pos = inicio;
 
   if (ListaSimplesVazia(*pos))
-    return(false);
+    return (false);
 
-  while (!ListaSimplesVazia((*pos)->prox)){
-    *ant   = (*pos);
-    *pos   = (*pos)->prox;    
+  while (!ListaSimplesVazia((*pos)->prox))
+  {
+    *ant = (*pos);
+    *pos = (*pos)->prox;
   }
 
-  return(true);
+  return (true);
 }
 
 NoListaSimples *CriaNoSimples(int elem)
 {
-  NoListaSimples *no = (NoListaSimples *)calloc(1,sizeof(NoListaSimples));
-  
+  NoListaSimples *no = (NoListaSimples *)calloc(1, sizeof(NoListaSimples));
+
   no->elem = elem;
   no->prox = NULL;
-  
-  return(no);
+
+  return (no);
 }
 
 void InsereInicioListaSimples(ListaSimples **inicio, int elem)
 {
-    while((*inicio)->prox != NULL){
-       inicio = &((*inicio)->prox);
-    }
-    NoListaSimples * inserindo = criaNoSimples(elem);
-    (*inicio)->prox = inserindo;
+  while ((*inicio)->prox != NULL)
+  {
+    inicio = &((*inicio)->prox);
+  }
+  NoListaSimples *inserindo = criaNoSimples(elem);
+  (*inicio)->prox = inserindo;
 }
 
 void InsereFimListaSimples(ListaSimples **inicio, int elem)
 {
-    while((*inicio)->prox != NULL){
-       inicio = &((*inicio)->prox);
-    }
-    ListaSimples * inserindo = (ListaSimples *)calloc(1, sizeof(ListaSimples));
-    inserindo->elem = elem;
-    (*inicio)->prox = inserindo;
+  while ((*inicio)->prox != NULL)
+  {
+    inicio = &((*inicio)->prox);
+  }
+  ListaSimples *inserindo = (ListaSimples *)calloc(1, sizeof(ListaSimples));
+  inserindo->elem = elem;
+  (*inicio)->prox = inserindo;
 }
 
 bool InsereElementoAntesDaChaveListaSimples(ListaSimples **inicio, int elem, int chave)
 {
 
-      /* preencher */
+  /* preencher */
 }
 
-bool  RemoveInicioListaSimples(ListaSimples **inicio, int *elem)
+bool RemoveInicioListaSimples(ListaSimples **inicio, int *elem)
 {
-  if(ListaSimplesVazia(&(*inicio)) == true){
-    return(false);
-  } else {
-    NoListaSimples * P = *inicio;
+  if (ListaSimplesVazia(&(*inicio)) == true)
+  {
+    return (false);
+  }
+  else
+  {
+    NoListaSimples *P = *inicio;
     *inicio = ((*inicio)->prox);
     free(P);
-    return(true);
-    //Falta colocar o *elem (retornar o que deletou pra variavel na main) e o false (vazio) e true (deletou) no return()
+    return (true);
+    // Falta colocar o *elem (retornar o que deletou pra variavel na main) e o false (vazio) e true (deletou) no return()
   }
 }
 
 bool RemoveElementoListaSimples(ListaSimples **inicio, int elem)
 {
-      /* preencher */
+  /* preencher */
 }
 
-bool  RemoveFimListaSimples(ListaSimples **inicio, int *elem)
+bool RemoveFimListaSimples(ListaSimples **inicio, int *elem)
 {
-  if(ListaSimplesVazia(&(*inicio)) == true){
-    return(false);
-  } else {
-  while((*inicio)->prox != NULL){
-    inicio = &((*inicio)->prox);
+  if (ListaSimplesVazia(&(*inicio)) == true)
+  {
+    return (false);
   }
-  *elem = (*inicio)->elem;
-  free((*inicio)->prox);
-  (*inicio)->prox = NULL;
-  return(true);
+  else
+  {
+    while ((*inicio)->prox != NULL)
+    {
+      inicio = &((*inicio)->prox);
+    }
+    *elem = (*inicio)->elem;
+    free((*inicio)->prox);
+    (*inicio)->prox = NULL;
+    return (true);
   }
 }
 
 void DestroiListaSimples(ListaSimples **inicio)
 {
   int elem;
-  while(!ListaSimplesVazia(*inicio))
-    RemoveInicioListaSimples(inicio,&elem);
-
+  while (!ListaSimplesVazia(*inicio))
+    RemoveInicioListaSimples(inicio, &elem);
 }
--- misc/myfunctions/my_nodes.c
+++ misc/myfunctions/my_nodes.c
@@ -1,64 +1,77 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-typedef struct NoSimples{
+typedef struct NoSimples
+{
     int content;
-    struct NoSimples * prox;
+    struct NoSimples *prox;
 } NoSimples;
 
-NoSimples * criaNoSimples();
+NoSimples *criaNoSimples();
 void insereNoSimplesFinal(NoSimples **P, int x);
 void insereNoSimplesInicio(NoSimples **P, int x);
 void removeInicio(NoSimples **inicio);
 bool ListaSimplesVazia(NoSimples *inicio);
 bool RemoveFimListaSimples(NoSimples **inicio, int *elem);
 
-NoSimples * criaNoSimples(int x){
-    NoSimples * no = (NoSimples *)calloc(1, sizeof(NoSimples));
+NoSimples *criaNoSimples(int x)
+{
+    NoSimples *no = (NoSimples *)calloc(1, sizeof(NoSimples));
     no->content = x;
     no->prox = NULL;
-    return(no);
+    return (no);
 }
-void removeInicio(NoSimples **inicio){
-    NoSimples * P = *inicio;
+void removeInicio(NoSimples **inicio)
+{
+    NoSimples *P = *inicio;
     *inicio = ((*inicio)->prox);
     free(P);
 }
-void insereNoSimplesFinal(NoSimples **inicio, int x){
-    while((*inicio)->prox != NULL){
-       inicio = &((*inicio)->prox);
+void insereNoSimplesFinal(NoSimples **inicio, int x)
+{
+    while ((*inicio)->prox != NULL)
+    {
+        inicio = &((*inicio)->prox);
     }
-    NoSimples * inserindo = criaNoSimples(x);
+    NoSimples *inserindo = criaNoSimples(x);
     (*inicio)->prox = inserindo;
 }
-void insereNoSimplesInicio(NoSimples **inicio, int x){
-    NoSimples * inserindo = criaNoSimples(x);
+void insereNoSimplesInicio(NoSimples **inicio, int x)
+{
+    NoSimples *inserindo = criaNoSimples(x);
     inserindo->prox = *inicio;
     (*inicio) = inserindo;
 }
-bool ListaSimplesVazia(NoSimples *inicio){
-  if (inicio == NULL) 
-    return(true);
-  else
-    return(false);
+bool ListaSimplesVazia(NoSimples *inicio)
+{
+    if (inicio == NULL)
+        return (true);
+    else
+        return (false);
 }
-bool RemoveFimListaSimples(NoSimples **inicio, int *elem){
-  if(ListaSimplesVazia(inicio) == true){
-    return(false);
-  } else {
-  while((*inicio)->prox != NULL){
-    inicio = &((*inicio)->prox);
-  }
-  *elem = (*inicio)->content;
-  free((*inicio)->prox);
-  (*inicio)->prox = NULL;
-  return(true);
-  }
+bool RemoveFimListaSimples(NoSimples **inicio, int *elem)
+{
+    if (ListaSimplesVazia(inicio) == true)
+    {
+        return (false);
+    }
+    else
+    {
+        while ((*inicio)->prox != NULL)
+        {
+            inicio = &((*inicio)->prox);
+        }
+        *elem = (*inicio)->content;
+        free((*inicio)->prox);
+        (*inicio)->prox = NULL;
+        return (true);
+    }
 }
 
-int main(){
-    NoSimples * P = criaNoSimples(1);
+int main()
+{
+    NoSimples *P = criaNoSimples(1);
     insereNoSimplesFinal(&P, 2);
     int elem;
     RemoveFimListaSimples(&P, &elem);
