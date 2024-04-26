788de12cb83e08fcf66cdcc12b4f6355592346f0
gabriel-braga-uc <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-04-26 00:01:56 -0300

    RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- labs/lab02 ~ verifica saidas de labirintos/main.c
+++ labs/lab02 ~ verifica saidas de labirintos/main.c
@@ -12,15 +12,12 @@ typedef struct _labirinto {
   Ponto saidas[200];
   Ponto entrada, saida;
 } Labirinto;
-Labirinto* LeLabirinto(char* nomearq)
-{
+
+Labirinto* LeLabirinto(char* nomearq){  
   FILE* fp = fopen(nomearq, "r");
   Labirinto* L = (Labirinto*)calloc(1, sizeof(Labirinto));
 
   fscanf(fp, "%d %d %d %d\n", &L->nx, &L->ny, &L->nz, &L->ne);
-  L->nx = L->nx - 1;
-  L->ny = L->ny - 1;
-  L->nz = L->nz - 1;
   L->Posicao = (char***)calloc(L->nz, sizeof(char**));
   for (int z = 0; z < L->nz; z++) {
     L->Posicao[z] = (char**)calloc(L->ny, sizeof(char*));
@@ -28,27 +25,38 @@ Labirinto* LeLabirinto(char* nomearq)
       L->Posicao[z][y] = (char*)calloc(L->nx, sizeof(char));
     }
   }
-  int aux = 0;
+  int aux_e = 0;
+  int aux_s = 0;
+  //printf("%d", L->nz);
   for (int z = 0; z < L->nz; z++) {
     for (int y = 0; y < L->ny; y++) {
+      //printf("\n");
       for (int x = 0; x < L->nx; x++) {
         fscanf(fp, "%c", &L->Posicao[z][y][x]);
+        //printf("%c", L->Posicao[z][y][x]);
         if (L->Posicao[z][y][x] == 'E') {
-          L->entradas[aux].x = x; L->entradas[aux].y = y; L->entradas[aux].z = z;
-          L->Posicao[z][y][x] = 'P';
+          //printf("\n <!> DEBUG E <!>\n");
+          L->entradas[aux_e].x = x; L->entradas[aux_e].y = y; L->entradas[aux_e].z = z;
+          //L->Posicao[z][y][x] = 'P';
+          aux_e++;
         }
         else {
           if (L->Posicao[z][y][x] == 'S') {
-            L->saidas[aux].x = x; L->saidas[aux].y = y; L->saidas[aux].z = z;
+            //printf("\n <!> DEBUG S <!>\n");
             L->ns++;
+            L->saidas[aux_s].x = x;
+            L->saidas[aux_s].y = y;
+            L->saidas[aux_s].z = z;
+            aux_s++;
             L->Posicao[z][y][x] = 'P';
           }
         }
+        fscanf(fp, "\n");
       }
-      fscanf(fp, "\n");
     }
   }
-  printf("\nnx %d, ny %d, nz %d, ne %d, ns %d", L->nx, L->ny, L->nz, L->ne, L->ns);
+  //int www = 5;
+  //("\nnx %d, ny %d, nz %d, ne %d, ns %d\n", L->nx, L->ny, L->nz, L->ne, L->ns);
   fclose(fp);
   return(L);
 }
@@ -66,52 +74,70 @@ void DestroiLabirinto(Labirinto** L)
     }
   }
 }
-bool Backtrack(Labirinto* L, Ponto P)
-{
-  bool res = false;
-  if ((P.z == L->saida.z) && (P.y == L->saida.y) && (P.x == L->saida.x)) {
-    res = true;
-  }
-  else {
-    Ponto* C = (Ponto*)calloc(4, sizeof(Ponto));
-    C[0].x = P.x - 1; C[0].y = P.y; C[0].z = P.z;
-    C[1].x = P.x + 1; C[1].y = P.y; C[1].z = P.z;
-    C[2].x = P.x; C[2].y = P.y - 1; C[2].z = P.z;
-    C[3].x = P.x; C[3].y = P.y + 1; C[3].z = P.z;
-    C[4].x = P.x; C[4].y = P.y; C[4].z = P.z - 1;
-    C[5].x = P.x; C[5].y = P.y; C[5].z = P.z + 1;
 
+
+bool Backtrack(Labirinto* L, Ponto P){
+  //printf("\n <!> DEBUG <!>\n");
+  bool res = false;
+  for(int k = 0; k < L->ns; k++){
+      L->saida.x = L->saidas[k].x; L->saida.y = L->saidas[k].y; L->saida.z = L->saidas[k].z;
+      if ((P.z == L->saida.z) && (P.y == L->saida.y) && (P.x == L->saida.x)) {
+        return(true);
+      }
+    }
+    Ponto* C = (Ponto*)calloc(6, sizeof(Ponto));
+    C[0].x = P.x - 1;
+    C[0].y = P.y;
+    C[0].z = P.z;
+    C[1].x = P.x + 1;
+    C[1].y = P.y;
+    C[1].z = P.z;
+    C[2].x = P.x;
+    C[2].y = P.y - 1;
+    C[2].z = P.z;
+    C[3].x = P.x;
+    C[3].y = P.y + 1;
+    C[3].z = P.z;
+    C[4].x = P.x;
+    C[4].y = P.y;
+    C[4].z = P.z - 1;
+    C[5].x = P.x;
+    C[5].y = P.y;
+    C[5].z = P.z + 1;
     char valor_original = L->Posicao[P.z][P.y][P.x];
     L->Posicao[P.z][P.y][P.x] = 'X';
-    for (int i = 0; (i < 7) && (res != true); i++) {
+    for (int i = 0; (i < 6) && (res != true); i++) {
+      //printf("\n!!!!!\n");
       if ((C[i].x >= 0) && (C[i].x < L->nx) && (C[i].y >= 0) && (C[i].y < L->ny) && (C[i].z >= 0) && (C[i].z < L->nz)) {
         if (L->Posicao[C[i].z][C[i].y][C[i].x] != 'X') {
+          //printf("! ");
           res = Backtrack(L, C[i]);
         }
       }
     }
-    L->Posicao[P.z][P.y][P.x] = valor_original;
-    free(C);
-  }
-  printf("test1");
+  L->Posicao[P.z][P.y][P.x] = valor_original;
+  free(C);
   return(res);
 }
 
 int main(int argc, char** argv)
 {
-  //for (int i = 10; i > 0; i--) {
-  //  printf("Sexo %d", i);
-  //}
   Labirinto* L = NULL;
   L = LeLabirinto(argv[1]);
+  //for(int k = 0; k < L->ns; k++){
+  //  printf("\n%d, %d, %d\n", L->saidas[k].x, L->saidas[k].y, L->saidas[k].z);
+  //}
   for (int i = 0; i < L->ne; i++) {
-    if (Backtrack(L, L->entradas[i])) {
-      printf("[Entrada %d] Saída encontrada!)\n", i);
+    L->entrada.x = L->entradas[i].x;
+    L->entrada.y = L->entradas[i].y;
+    L->entrada.z = L->entradas[i].z;
+    //printf("E: %d, %d, %d\n", L->entrada.x, L->entrada.y, L->entradas[i].z);
+    if (Backtrack(L, L->entrada)) {
+      printf("[Entrada %d] Saída encontrada!\n", i);
     }
     else {
-      printf("[Entrada %d] Saída não encontrada!)\n", i);
-    }
+      printf("[Entrada %d] Saída não encontrada!\n", i);
+      }
   }
-  printf("test");
   return(0);
 }
