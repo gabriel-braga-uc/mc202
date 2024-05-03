d31268931133769f5a6524bd2513c0049b597d99
gabriel-braga-uc <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-05-03 20:18:48 -0300

    1
    1

---
+++ falcao code/9. Arvore Binária Construção e percursos/myarvores.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _ArvoreBinaria{
+    char info;
+    struct _ArvoreBinaria *dir, *esq;
+} Arvore, NoArvore;
+
+
+int main(){
+
+    return 0;
+}
